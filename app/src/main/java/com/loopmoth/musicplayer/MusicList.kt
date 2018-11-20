288435ec0317b5b7d74321631ad61374fa9bea12
MatthiasPL <smateusz96@gmail.com>
2018-11-20 09:39:29 +0100

    Initial commit

---
+++ app/src/main/java/com/loopmoth/musicplayer/MusicList.kt
@@ -0,0 +1,12 @@
+package com.loopmoth.musicplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_list)
+    }
+}
---
+++ app/src/main/java/com/loopmoth/musicplayer/MusicPlayer.kt
@@ -0,0 +1,12 @@
+package com.loopmoth.musicplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicPlayer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+    }
+}
